4882e725b047ab7dd5f060dd8faaa0759301f8d9
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-10-15 00:14:19 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
+import {ref,push,child,update} from "firebase/database";
+import database from './firebase';
+function RegistrationForm() {
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password,setPassword] = useState(null);
+  const [confirmPassword,setConfirmPassword] = useState(null);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const handleInputChange = (e) => {
+    const {id , value} = e.target;
+    if(id === "firstName"){
+        setFirstName(value);
+    }
+    if(id === "lastName"){
+        setLastName(value);
+    }
+    if(id === "email"){
+        setEmail(value);
+    }
+    if(id === "password"){
+        setPassword(value);
+    }
+    if(id === "confirmPassword"){
+        setConfirmPassword(value);
+    }
+
+}
+const handleSubmit  = () => {
+  console.log(firstName,lastName,email,password,confirmPassword);
+  let obj = {
+    firstName : firstName,
+    lastName:lastName,
+    email:email,
+    password:password,
+    confirmPassword:confirmPassword,
+}       
+const newPostKey = push(child(ref(database), 'posts')).key;
+const updates = {};
+updates['/' + newPostKey] = obj
+return update(ref(database), updates);
 }
 
-export default App;
+return(
+  <div className="form">
+      <div className="form-body">
+          <div className="username">
+              <label className="form__label" for="firstName">First Name </label>
+              <input className="form__input" type="text" value={firstName} onChange = {(e) => handleInputChange(e)} id="firstName" placeholder="First Name"/>
+          </div>
+          <div className="lastname">
+              <label className="form__label" for="lastName">Last Name </label>
+              <input  type="text" name="" id="lastName" value={lastName}  className="form__input" onChange = {(e) => handleInputChange(e)} placeholder="LastName"/>
+          </div>
+          <div className="email">
+              <label className="form__label" for="email">Email </label>
+              <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
+          </div>
+          <div className="password">
+              <label className="form__label" for="password">Password </label>
+              <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
+          </div>
+          <div className="confirm-password">
+              <label className="form__label" for="confirmPassword">Confirm Password </label>
+              <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} placeholder="Confirm Password"/>
+          </div>
+      </div>
+      <div class="footer">
+          <button onClick={()=>handleSubmit()} type="submit" class="btn">Register</button>
+      </div>
+  </div>
+ 
+)       
+}
+
+export default RegistrationForm;
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { getDatabase } from "firebase/database"
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqegu5VFcbHHNSj_gwzfk5IiND3IIBXwg",
+    authDomain: "event-f4b6c.firebaseapp.com",
+    projectId: "event-f4b6c",
+    storageBucket: "event-f4b6c.appspot.com",
+    messagingSenderId: "289449101950",
+    appId: "1:289449101950:web:b14ac3a96b126c914ee62f",
+    measurementId: "G-6TMD3WYS0P"
+  };
+  
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
+const database = getDatabase(app);
+export default database;
