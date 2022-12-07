ff42852b05a302fa51561aec9cf62f0ce4e183a9
Sanjay Kumar M <sanjaymoad786@gmail.com>
2022-12-07 21:57:24 +0530

    security

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -2,12 +2,12 @@ import { getDatabase } from "firebase/database"
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqegu5VFcbHHNSj_gwzfk5IiND3IIBXwg",
+  apiKey: "/////",
   authDomain: "event-f4b6c.firebaseapp.com",
   projectId: "event-f4b6c",
   storageBucket: "event-f4b6c.appspot.com",
   messagingSenderId: "289449101950",
-  appId: "1:289449101950:web:b14ac3a96b126c914ee62f",
+  appId: "////",
   measurementId: "G-6TMD3WYS0P"
 };
 
